c20d3f8a15211598ba613f4b543bc1e2213b474c
Pedro Cantarin <pedro.cantarin98@gmail.com>
2026-03-31 12:59:50 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/controllers/UserController.java
+++ src/main/java/com/educandoweb/course/controllers/UserController.java
@@ -1,19 +1,33 @@
 package com.educandoweb.course.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User user = new User(1L, "Pedro", "pedro@gmail.com", "(19) 99999-9999", "12345");
+	public ResponseEntity<List<User>> findAll(){
+		List<User> users = service.findAll();
+		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
 	
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -1,5 +1,38 @@
 package com.educandoweb.course.services;
 
-public class UserService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	/**
+	 * Retrieves all users registered in the database.
+	 *
+	 * @return a list of all users
+	 */
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	/**
+	 * Retrieves a User by its unique identifier.
+	 *
+	 * @param id the ID of the user to be retrieved
+	 * @return the User entity corresponding to the given ID
+	 */
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
 }
